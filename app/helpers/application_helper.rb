c40e6a71e592b606daf0167f3c2d654301496b07
Bradley Hazelton <bradley@bradnet.net>
2017-12-29 19:11:06 -0500

    style fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,15 @@ module ApplicationHelper
   def club_name
     "Calalta Figure Skating Club"
   end
+  
   def site_url
     "http://calalta.walloffame.ca"
   end
+  
   def club_abb
     "CFSC"
   end
+  
   def club_short
     "Calalta"
   end
